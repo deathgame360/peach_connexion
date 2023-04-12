964ac179e9ed1d2c810df1c12395955ddc8cf2af
deathgame360 <ol08.fp@gmail.com>
2023-04-12 23:10:06 +0200

    MAJ
    ajout de code titre ,fond et thème

--- peach.main.py
+++ peach.main.py
@@ -3,12 +3,16 @@ from tkinter import messagebox
 import customtkinter
 import tkinter
 
+customtkinter.set_appearance_mode('Dark')
+customtkinter.set_default_color_theme("green")
 
-peach_connection = Tk()
+
+peach_connection = customtkinter.CTk()
+
 peach_connection.title('Peach connexion ')
 peach_connection.geometry("500x500")
 peach_connection.configure(bg="blue")
-peach_connection.resizable()
+peach_connection.title('Devis')
 
 
 peach_connection.mainloop()
