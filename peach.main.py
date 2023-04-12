674b005a9cd9f16b7a38ca5c703ab9a4bd7d3343
deathgame360 <ol08.fp@gmail.com>
2023-04-12 22:12:33 +0200

    MAJ
    modification de resizable mais toujours une erreur

--- peach.main.py
+++ peach.main.py
@@ -6,9 +6,9 @@ import tkinter
 
 peach_connection = Tk()
 peach_connection.title('Peach connexion ')
-peach_connection.geoimetry("500x500")
+peach_connection.geometry("500x500")
 peach_connection.configure(bg="blue")
-peach_connection.resizable(False)
+peach_connection.resizable()
 
 
 peach_connection.mainloop()
