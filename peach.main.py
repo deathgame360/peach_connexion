3bf7250ab8c04b7d12ed166e16c47fe7039bb532
deathgame360 <ol08.fp@gmail.com>
2023-04-13 20:14:24 +0200

    photo et code
    Ajout de trois photo et de code python pour les font d'accueil

--- peach.main.py
+++ peach.main.py
@@ -15,9 +15,19 @@ peach_connection.geometry("500x500")
 peach_connection.configure(bg="blue")
 peach_connection.title('Devis')
 
-image_de_fond = ImageTk.PhotoImage(Image.open(""))
-l1 = customtkinter.CTkLabel(master=peach_connection , image = image_de_fond)
-l1.pack(fill='both')
+image_de_fond = ImageTk.PhotoImage(Image.open("manga_marché.jpeg"))
+font = customtkinter.CTkLabel(master=peach_connection, image=image_de_fond, text="heart beam")
+font.pack(fill='both')
+
+image_de_droite=ImageTk.PhotoImage(Image.open("manga_women.jpeg"))
+font_droite=customtkinter.CTkLabel(master=peach_connection, image=image_de_droite, texte=None)
+font_droite.pack(fill='right')
+
+image_de_gauche=ImageTk.PhotoImage(Image.open("woomen drag.jpg"))
+font_gauche=customtkinter.CTkLabel(master=peach_connection, image=image_de_gauche, texte=None)
+font_gauche.pack(fill='left')
+
+
 
 
 peach_connection.mainloop()
