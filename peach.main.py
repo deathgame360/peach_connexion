ac1719c3357aff43828dfac8536ccf83c8f6b1e1
deathgame360 <ol08.fp@gmail.com>
2023-04-18 19:19:03 +0200

    MAJ image et code
    Ajout d'une nouvelle image +modification de la taille
    et de la disposition des autres pour créer un petit menue au milieux

--- peach.main.py
+++ peach.main.py
@@ -19,10 +19,12 @@ peach_connection.title('Devis')
 resize_IG = Image.open("manga_women.jpeg")
 resize_ID = Image.open("marche.jpeg")
 resize_IF = Image.open("woomen drag.jpg")
+resize_MM = Image.open("manga_marché.jpeg")
 
-marche = resize_IG.resize((500, 800), Image.LANCZOS)
-manga_women = resize_ID.resize((500, 800), Image.LANCZOS)
-woomen_drag = resize_IF.resize((500, 800), Image.LANCZOS)
+marche = resize_IG.resize((600, 900), Image.LANCZOS)
+manga_women = resize_ID.resize((200, 400), Image.LANCZOS)
+woomen_drag = resize_IF.resize((600, 900), Image.LANCZOS)
+manga_marché = resize_MM.resize((200, 400), Image.LANCZOS)
 
 image_de_fond = ImageTk.PhotoImage(woomen_drag)
 font = customtkinter.CTkLabel(
@@ -32,12 +34,17 @@ font.pack(side=LEFT, padx=15, pady=0)
 image_de_droite = ImageTk.PhotoImage(marche)
 font_droite = customtkinter.CTkLabel(
     master=peach_connection, image=image_de_droite, height=100, width=100, text="", text_color="blue")
-font_droite.pack(side=LEFT, padx=15, pady=0)
+font_droite.pack(side=RIGHT, padx=15, pady=0)
+
+image_de_droite_2 = ImageTk.PhotoImage(manga_marché)
+font_droite = customtkinter.CTkLabel(
+    master=peach_connection, image=image_de_droite_2, height=100, width=100, text="", text_color="blue")
+font_droite.pack(side=TOP, padx=15, pady=0)
 
 image_de_gauche = ImageTk.PhotoImage(manga_women)
 font_gauche = customtkinter.CTkLabel(
     master=peach_connection, image=image_de_gauche, height=100, width=100, text="", text_color="blue")
-font_gauche.pack(side=LEFT, padx=15, pady=0)
+font_gauche.pack(side=BOTTOM, padx=15, pady=0)
 
 
 frame_connexion = customtkinter.CTkFrame(
